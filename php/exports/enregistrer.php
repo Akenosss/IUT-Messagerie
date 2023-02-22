7d081ead9c38ad6e30f038ec200187e7965fbd5e
Ribot Leo <leo.ribot@etu.iut-tlse3.fr>
2023-02-22 12:17:13 +0100

    ft(login.php&database.php) add new classes enregistrer.php work recuperer.php work

--- php/exports/enregistrer.php
+++ php/exports/enregistrer.php
@@ -1,23 +1,23 @@
 <?php
-    /* $user = htmlspecialchars($_GET['user']);
-    $content = htmlspecialchars($_GET['content']); */
+    $user = htmlspecialchars($_GET['user']);
+    $content = htmlspecialchars($_GET['content']);
 
-    $user = "oui";
-    $content = "oui";
-
+    use config\login;
+    use models\Database;
     include_once '../config/login.php';
     include_once '../models/Database.php';
+
 
     $database = new login();
     $db = $database->getConnection();
 
-    $phrases = new \models\Database($db);
+    $input = new Database($db);
 
     if(!empty($user) && !empty($content)) {
-        $phrases->user  = $user;
-        $phrases->content  = $content;
+        $input->user  = $user;
+        $input->content  = $content;
 
-        if($phrases->post()) {
+        if($input->post()) {
             http_response_code(201);
             echo json_encode(["message" => "Done"]);
         } else {
---
+++ php/models/Database.php
@@ -0,0 +1,47 @@
+<?php
+namespace models;
+
+class Database {
+    private $connection;
+    private $table = "chat";
+
+    public $id;
+    public $date_message;
+    public $user;
+    public $content;
+
+    public function __construct($db) {
+        $this->connection = $db;
+    }
+
+    public function getAll()
+    {
+        $sql = "SELECT * from " . $this->table;
+        $query = $this->connection->prepare($sql);
+        $query->execute();
+
+        return $query;
+    }
+
+    public function post()
+    {
+        $sql = "INSERT INTO " .$this->table. " SET date_message = NOW(), user = :user, content = :content";
+
+        //  Préparation requête
+        $query = $this->connection->prepare($sql);
+
+        // Protection contre les injections
+        $this->user     = htmlspecialchars((strip_tags($this->user)));
+        $this->content  = htmlspecialchars((strip_tags($this->content)));
+
+        // Ajout des données protégées
+        $query->bindParam(":user", $this->user);
+        $query->bindParam(":content", $this->content);
+
+        // Exec req
+        if ($query->execute()) {
+            return true;
+        }
+        return false;
+    }
+}
