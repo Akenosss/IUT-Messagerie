962ceef37eadd642d9cc27c62cf3e7474cdbe4e0
Ribot Leo <leo.ribot@etu.iut-tlse3.fr>
2023-03-17 23:47:59 +0100

    fix(body) fix body css and structure

--- src/main/ressources/templates/listmessages.php
+++ src/main/ressources/templates/listmessages.php
@@ -4,8 +4,12 @@
 
     foreach ($tab['data'] as $t):
 ?>
-
-    <p><span><strong><?= $t['user']?></strong></span>
-    <span class="message"><?= $t['content']?></span></p>
+    <div class="main-content">
+        <div class="content">
+            <span><strong><?= $t['user']?></strong></span>
+            <span class="message"><?= $t['content']?></span>
+        </div>
+        <span><?= $t['date_message'] ?></span>
+    </div>
 
 <?php endforeach; ?>
---
+++ src/main/ressources/templates/sidebar.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link href='../static/css/sidebar.css' rel='stylesheet'>
+    <script src="../static/js/sidebar.js"></script>
+    <title>Sidebar</title>
+</head>
+<body>
+    <div class="sidebar">
+        <div class="menu">
+            <i class='bx bx-menu' id="btn"></i>
+        </div>
+        <div class="search-bar">
+            <i class='bx bx-search-alt'></i>
+            <input type="text" placeholder="Search..."/>
+        </div>
+        <div class="main-sidebar">
+            <div class="nav-list">
+                <a href="#" id="block">
+                    <i class='bx bx-user-circle'></i>
+                    <span class="links-name">User name</span>
+                </a>
+            </div>
+            <div class="current-user">
+                <span>MOI</span>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
